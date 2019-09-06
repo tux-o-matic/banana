f69196ab642463f487524511b60e8227cfe303e2
Benjamin Merot <4562092+tux-o-matic@users.noreply.github.com>
2019-09-06 10:52:45 +0200

    Made cifar10 run with plaidml behind Keras
    TensorBoard can't be used with the plaidml backend and there for had to be removed.

--- cifar10_cnn.py
+++ cifar10_cnn.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 
+import os
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 import keras
 import numpy as np
 from keras import backend as K
 from keras import optimizers
 from keras import regularizers
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.datasets import cifar10
 from keras.layers.convolutional import MaxPooling2D
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, BatchNormalization, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
-import os
 import sys
 
 # Avoid warnings about CPU features
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-batch_size = 32
+batch_size = 129
 nb_classes = 10
 nb_epoch = 100
 saved_model = os.path.join(os.getcwd(), 'keras_cifar10_trained_model.h5')
@@ -79,12 +80,11 @@ sgd = optimizers.SGD(lr=lrate, decay=decay_rate, momentum=0.9, nesterov=True)
 lr_reducer = ReduceLROnPlateau(monitor='val_acc', factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=1e-5, verbose=1)
 early_stopping_callback = EarlyStopping(monitor='val_acc', patience=10)
 model_checkpoint = ModelCheckpoint(saved_model, monitor="val_acc", save_best_only=True, verbose=1)
-board = TensorBoard(log_dir='./logs', histogram_freq=x_train.shape[0], batch_size=batch_size, write_grads=True)
 
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 model.fit_generator(generator.flow(x_train, y_train, batch_size=batch_size), epochs=nb_epoch, verbose=1, workers=3,
                     use_multiprocessing=True, validation_data=(x_test, y_test), steps_per_epoch=x_train.shape[0]/nb_classes,
-                    callbacks=[board, lr_reducer, early_stopping_callback, model_checkpoint])
+                    callbacks=[lr_reducer, early_stopping_callback, model_checkpoint])
 score = model.evaluate(x_test, y_test, verbose=1)
 print('\nTest result: %.3f loss: %.3f' % (score[1]*100, score[0]))
 model.save(saved_model)
