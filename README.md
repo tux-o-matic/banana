26b7ae13d2d622bcbcbbf73422e60ce9429d25e9
Ben <4562092+tux-o-matic@users.noreply.github.com>
2023-01-02 23:41:36 +0100

    Added NumPy install steps for Apple Silicon. PEP8 improvements

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-## Setup
-For training this CNN with over 200 steps and more than 10 epochs, a GPU with CUDA support is a must. CPU based training will take several hours.
+# Setup
+For training this CNN with over 200 steps and more than 10 epochs, a GPU is a must. CPU based training will take several hours but for portability reasons, the `requirements.txt` file installs `tensorflow-cpu` by default on systems other than macOS.
 
-At this time, all tests were done with CUDA 9.0.
-### Nvidia drivers
+All tests were done with Metal on Apple Silicon and CUDA 9.0 on Linux x86_64.
+
+## Metal
+pip will auto detect macOS and install the improved TensorFlow implementations from Apple to use Metal on Apple Silicon and x86_64 Mac with discrete AMD GPU. Integrated Intel GPU support Metal shaders but those TF backend didn't boost TF performance (Apple therefor gave up supporting Intel iGPU for the Metal backend) so those Mac should fallback to `tensorflow-cpu` or try the Intel distribution with further SSE/AVX optimization `intel-tensorflow`.
+
+### vecLib
+NumPy performance on Apple Silicon can be further improved by using [Apple's vecLib](https://developer.apple.com/documentation/accelerate/veclib) instead of the default OpenBLAS otherwise used by the arm64 NumPy wheel. A `site.cfg` file is present in the repository to pass hints when building NumPy.
+Within your virtualenv:
+```shell
+pip install Cython
+pip install -U --no-binary :all: --no-use-pep517 numpy
+```
+
+To verify, within an interactive Python interpreter in your virtualenv:
+```shell
+>>> import numpy
+>>> numpy.show_config()
+```
+You should see the vecLib headers listed.
+
+Based on [this post](https://developer.apple.com/forums/thread/695963).
+
+## Nvidia drivers
 You'll need the closed source Nvidia drivers, one supporting the correct CUDA version.
 Blacklist the Nouveau open source driver it's loaded at boot.
 ### CUDA
@@ -19,15 +40,21 @@ sudo cp cuda/lib64/* /usr/lib64/
 ### TensorFlow
 Rename the entry requirements.txt from
 ```ini
-tensorflow==2.1.0
+tensorflow-cpu=XXX
 ```
 to
 ```ini
-tensorflow-cpu==2.1.0
+tensorflow==XXX
 ```
-If you do not have an Nvidia GPU and which to use the lighter module which will run only on CPU.
-If your CPU doesn't support the AVX1 instruction set (Intel chip shipped before 2012), you'll need to use an older version of TensorFlow such as 1.5.0 (see keras-tf1 branch).
 
 
 ## Results
 So far training gave an accuracy of 78%.
+The more RAM and bandwith your GPU has, the larger batch it can handle. So the `batch_size` value should be tuned accordingly.
+
+## Known Issues
+Apple's Metal backend for Tensorflow has been broken for a few release after TensorFlow 2.9 and `tensorflow-metal` 0.5. So even though it might lack some MPS optimizations, those older versions can actually run.
+
+Apple's Metal optimized TensorFlow can have "likely leaking IOGPUResource" [errors](https://developer.apple.com/forums/thread/706920?login=true&page=1#741165022) after running for a few epochs.
+
+If your CPU doesn't support the AVX1 instruction set (Intel chip shipped before 2012), you'll need to use an older version of TensorFlow such as 1.5.0 (see keras-tf1 branch).
--- cifar10_cnn.py
+++ cifar10_cnn.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
 
+import os
 import numpy as np
 import tensorflow
 from tensorflow.keras import regularizers
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
+from tensorflow.keras.callbacks import BackupAndRestore, EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, BatchNormalization, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPool2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
-import sys
 
 # Avoid warnings about CPU features
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 print(tensorflow.test.gpu_device_name())
 
-batch_size = 32
-nb_classes = 10
-nb_epoch = 100
+
+class InterruptAtEpoch(tensorflow.keras.callbacks.Callback):
+    '''A callback for interrupting training since MPS has leaks over time'''
+    def __init__(self, interrupting_epoch=5):
+        self.interrupting_epoch = interrupting_epoch
+
+    def on_epoch_end(self, epoch, log=None):
+        '''Called at the end of an epoch'''
+        if (epoch % self.interrupting_epoch) == 0:
+            raise RuntimeError('Interruption')
+
+
+BATCH_SIZE = 16
+NB_CLASSES = 10
+NB_EPOCH = 100
 saved_model = os.path.join(os.getcwd(), 'keras_cifar10_trained_model.h5')
 
 
@@ -28,61 +39,78 @@ saved_model = os.path.join(os.getcwd(), 'keras_cifar10_trained_model.h5')
 x_train = x_train.astype('float32') / 255.0 - 0.5
 x_test = x_test.astype('float32') / 255.0 - 0.5
 
-y_train = tensorflow.keras.utils.to_categorical(y_train, nb_classes)
-y_test = tensorflow.keras.utils.to_categorical(y_test, nb_classes)
+y_train = tensorflow.keras.utils.to_categorical(y_train, NB_CLASSES)
+y_test = tensorflow.keras.utils.to_categorical(y_test, NB_CLASSES)
 
 _, img_channels, img_rows, img_cols = x_train.shape
 
-generator = ImageDataGenerator(rotation_range=90, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-generator.fit(x_train, seed=0)    
+generator = ImageDataGenerator(rotation_range=90, width_shift_range=0.1,
+                               height_shift_range=0.1, horizontal_flip=True)
+generator.fit(x_train, seed=0)   
 
-wdecay = 1e-4
-    
+WDECAY = 1e-4
+  
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same', kernel_regularizer=regularizers.l2(wdecay), input_shape=x_train.shape[1:]))
+model.add(Conv2D(32, (3, 3), padding='same',
+          kernel_regularizer=regularizers.l2(WDECAY),
+          input_shape=x_train.shape[1:]))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
-model.add(Conv2D(32, (3, 3), padding='same', kernel_regularizer=regularizers.l2(wdecay)))
+model.add(Conv2D(32, (3, 3), padding='same',
+          kernel_regularizer=regularizers.l2(WDECAY)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 
-model.add(Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(wdecay)))
+model.add(Conv2D(64, (3, 3), padding='same',
+          kernel_regularizer=regularizers.l2(WDECAY)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
-model.add(Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(wdecay)))
+model.add(Conv2D(64, (3, 3), padding='same',
+          kernel_regularizer=regularizers.l2(WDECAY)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Dropout(0.3))
 
-model.add(Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(wdecay)))
+model.add(Conv2D(128, (3, 3), padding='same',
+          kernel_regularizer=regularizers.l2(WDECAY)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
-model.add(Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(wdecay)))
+model.add(Conv2D(128, (3, 3), padding='same',
+          kernel_regularizer=regularizers.l2(WDECAY)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Dropout(0.4))
 
 model.add(Flatten())
-model.add(Dense(nb_classes, activation='softmax'))
+model.add(Dense(NB_CLASSES, activation='softmax'))
 
 model.summary()
 
-lrate = 0.001
-sgd = tensorflow.keras.optimizers.SGD(learning_rate=lrate, momentum=0.9, nesterov=True)
+LRATE = 0.001
+sgd = tensorflow.keras.optimizers.SGD(learning_rate=LRATE, momentum=0.9,
+                                      nesterov=True)
 
-lr_reducer = ReduceLROnPlateau(monitor='val_accuracy', factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=1e-5, verbose=1)
+lr_reducer = ReduceLROnPlateau(monitor='val_accuracy', factor=np.sqrt(0.1),
+                               cooldown=0, patience=5, min_lr=1e-5, verbose=1)
 early_stopping_callback = EarlyStopping(monitor='val_accuracy', patience=10)
-model_checkpoint = ModelCheckpoint(saved_model, monitor="val_accuracy", save_best_only=True, verbose=1)
+model_checkpoint = ModelCheckpoint(saved_model, monitor="val_accuracy",
+                                   save_best_only=True, verbose=1)
 board = TensorBoard(log_dir='./logs', histogram_freq=x_train.shape[0])
+backup_restore_callback = BackupAndRestore(backup_dir='/tmp')
 
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-model.fit(generator.flow(x_train, y_train, batch_size=batch_size), epochs=nb_epoch, verbose=1, workers=4,
-                    validation_data=(x_test, y_test), steps_per_epoch=x_train.shape[0]/nb_classes,
-                    callbacks=[board, lr_reducer, early_stopping_callback, model_checkpoint])
+model.compile(loss='categorical_crossentropy', optimizer=sgd,
+              metrics=['accuracy'])
+model.fit(generator.flow(x_train, y_train, batch_size=BATCH_SIZE),
+          epochs=NB_EPOCH, verbose=1, workers=4,
+          validation_data=(x_test, y_test),
+          steps_per_epoch=x_train.shape[0]/NB_CLASSES,
+          callbacks=[board, lr_reducer, early_stopping_callback,
+                     model_checkpoint, backup_restore_callback,
+                     InterruptAtEpoch()])
 score = model.evaluate(x_test, y_test, verbose=1)
-print('\nTest result: %.3f loss: %.3f' % (score[1]*100, score[0]))
+print(f"\nTest result: {score[1]*100} loss: {score[0]}")
 model.save(saved_model)
